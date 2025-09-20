7ce2b250f70517ccb4303769566d2b5d11395998
Noks19 <97792290+Noks19@users.noreply.github.com>
2025-09-20 16:49:39 +0200

    made changes to the html and js files

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-const inventory = [
-  ...bakery,
-  ...beverages,
-  ...cannedGoods,
-  ...dairy,
-  ...frozen,
-  ...produce,
-  ...snacks,
-  ...household,
-];
 console.log("Inventory loaded:", inventory.length);
