eeb026406c7a820f2dedf8c031f089ead8d5c906
Noks19 <97792290+Noks19@users.noreply.github.com>
2025-09-22 09:29:48 +0200

    updated

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-
-
 // const inventory = [
 //   ...bakery,
 //   ...beverages,
@@ -12,7 +9,6 @@
 //   ...household,
 // ];
 // console.log("Inventory loaded:", inventory.length);
-
 
 // Function to add product
 
@@ -46,7 +42,6 @@ function addProduct() {
     alert("Please provide a non-empty name.");
     return;
   }
-
 
   // Price
   let price = parseFloat(prompt("Please enter the price:"));
@@ -147,6 +142,110 @@ function removeProduct() {
 
 console.log("Updated Inventory:", inventory);
 
+// Update Stock
+function updateStockQuantity(id, changeAmount) {
+  const productId = prompt("Enter productId");
+  console.log(productId);
+
+  const quantityUpdate = prompt("Enter quantityUpdate");
+  console.log(quantityUpdate);
+
+  let index = inventory.findIndex(
+    (product) => product.id === Number(productId)
+  );
+  if (index !== -1) {
+    inventory[index].quantity = quantityUpdate;
+    console.log(
+      `stock for product ${inventory[index].name} ${inventory[index].id} updated to ${quantityUpdate}.`
+    );
+  } else {
+    console.log(`stock for product ${inventory[index].id} not found.`);
+  }
+}
+
+function generateReport() {
+  const totalProducts = inventory.length;
+  let totalValue = 0;
+  let lowStockItems = [];
+  let highestValueProduct = null;
+  let highestValue = 0;
+
+  for (let i = 0; i < inventory.length; i++) {
+    const product = inventory[i];
+
+    const stockInventory = [];
+
+    const addStockInventory = (name, category, id, price, quantity) => {
+      // local variable (local scope) inventory
+      const inventory = {
+        name: name,
+        category: category,
+        id: id,
+        price: price,
+        quantity: quantity,
+      };
+      stockInventory.push(inventory); // global variable // global scope
+    };
+
+    const findInventoryByName = (productId) => {
+      const inventory = stockInventory.find(function (inv) {
+        if (inv.productId === productId) {
+          return true;
+        }
+        return false;
+      });
+      return inventory;
+    };
+
+    const productValue = product.price * product.quantity;
+    totalValue += productValue;
+
+    if (product.quantity < 10) {
+      lowStockItems.push({
+        name: product.name,
+        quantity: product.quantity,
+      });
+    }
+
+    if (productValue > highestValue) {
+      highestValue = productValue;
+      highestValueProduct = product;
+    }
+  }
+
+  console.log(`Total products: ${totalProducts}`);
+  console.log(`Total Inventory Value: $${totalValue.toFixed(2)}`);
+
+  if (lowStockItems.length > 0) {
+    console.log("Low Stock Items:");
+    for (let Item of lowStockItems) {
+      console.log(`-${Item.name} (Quantity: ${Item.quantity})`);
+    }
+  } else {
+    console.log("No low stock item!");
+  }
+
+  if (highestValueProduct) {
+    console.log(
+      `highest value product: ${
+        highestValueProduct.name
+      } ($${highestValue.toFixed(2)})`
+    );
+  }
+
+  alert(
+    `Report:\n ` +
+      `Total products: ${totalProducts}\n` +
+      `Total Value: ${totalValue.toFixed(2)}\n` +
+      (highestValueProduct
+        ? `Highest value: ${highestValueProduct.name} ($${highestValue.toFixed(
+            2
+          )})\n`
+        : "") +
+      `Low Stock Items: ${lowStockItems.length}`
+  );
+}
+
 function inventoryApplication() {
   const choice = prompt(
     "Enter a command:\n1. Add Product\n2. Remove Product\n3. Update Stock\n4. Generate Report\n5. Exit"
@@ -172,168 +271,4 @@ function inventoryApplication() {
   }
 }
 
-
-//  addProduct(7738311193, "", "Coconut Bread", 2900, 50); //Throws a duplicate id + the empty category
-
-//     addProduct(9603410528, "Produce", "Cassava", -3500, 5); //Throws an error for the negative price
 inventoryApplication();
-
-generat - product;
-console.log("Inventory loaded:", inventory.length);
-
-
-function generateReport() {
-  const totalProducts = inventory.length;
-  let totalValue = 0;
-  let lowStockItems = [];
-  let highestValueProduct = null;
-  let highestValue = 0;
-
-  for (let i = 0; i < inventory.length; i++) {
-    const product = inventory[i];
-
-const stockInventory = [];
-
-const addStockInventory = (name, category, id, price, quantity) => {
-  // local variable (local scope) inventory
-  const inventory = {
-    name: name,
-    category: category,
-    id: id,
-    price: price,
-    quantity: quantity,
-  };
-  stockInventory.push(inventory); // global variable // global scope
-};
-
-const findInventoryByName = (productId) => {
-  const inventory = stockInventory.find(function(inv)
-  {if (inv.productId === productId) {
-    return true;
-  }
-  return false;
-});
-return inventory;
-};
-
-
-// Update Stock 
-function updateStockQuantity(id, changeAmount) {
-
-  const productId = prompt("Enter productId")
-  console.log(productId)
-
-  const quantityUpdate = prompt("Enter quantityUpdate")
-  console.log(quantityUpdate)
-
-  let index = inventory.findIndex(product => product.id === Number(productId))
-if (index !== -1){
-  inventory[index].quantity = quantityUpdate
-  console.log(`stock for product ${inventory[index].name} ${inventory[index].id} updated to ${quantityUpdate}.`);
-} else {
-  console.log(`stock for product ${inventory[index].id} not found.`);
-}
-
-}
-  
-
-    const productValue = product.price * product.quantity;
-    totalValue += productValue;
-
-    if (product.quantity < 10) {
-      lowStockItems.push({
-        name: product.name,
-        quantity: product.quantity,
-      });
-    }
-
-    if (productValue > highestValue) {
-      highestValue = productValue;
-      highestValueProduct = product;
-    }
-  }
-
-  console.log(`Total products: ${totalProducts}`);
-  console.log(`Total Inventory Value: $${totalValue.toFixed(2)}`);
-
-
-  if (lowStockItems.length > 0) {
-    console.log("Low Stock Items:");
-    for (let Item of lowStockItems) {
-      console.log(`-${Item.name} (Quantity: ${Item.quantity})`);
-    }
-  } else {
-    console.log("No low stock item!");
-  }
-
-  if (highestValueProduct) {
-    console.log(
-      `highest value product: ${
-        highestValueProduct.name
-      } ($${highestValue.toFixed(2)})`
-    );
-  }
-
-<<<<<<< Updated upstream
-  alert(
-    `Report:\n ` +
-      `Total products: ${totalProducts}\n` +
-      `Total Value: ${totalValue.toFixed(2)}\n` +
-      (highestValueProduct
-        ? `Highest value: ${highestValueProduct.name} ($${highestValue.toFixed(
-            2
-          )})\n`
-        : "") +
-      `Low Stock Items: ${lowStockItems.length}`
-  );
-}
-
-function mainMenu() {
-  let choice;
-
-  do {
-    choice = prompt(
-      "Inventory Management Menu:\n" +
-        "1. Add Product \n" +
-        "2. Remove Product \n" +
-        "3. Update Stock\n" +
-        "4. Generate Report\n" +
-        "5. Exit\n\n" +
-        "Enter your choice (1-5):"
-    );
-
-    if (choice === null) {
-      break;
-    }
-
-    switch (choice.trim()) {
-      case "1":
-        alert("Add Product feature.");
-        break;
-
-      case "2":
-        alert("Remove Product feature.");
-        break;
-
-      case "3":
-        alert("Update Stock feature.");
-        break;
-
-      case "4":
-        generateReport();
-        break;
-
-      case "5":
-        alert("Exiting Inventory Management System.");
-        break;
-
-      default:
-        alert("Invalid choice. Please enter 1-5.");
-        break;
-    }
-  } while (choice !== "5" && choice !== null);
-}
-
-=======
- 
->>>>>>> Stashed changes
